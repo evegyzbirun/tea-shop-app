543a82850a06a6d2735d661e070a09af62a635a9
evegyzbirun <zbirunzbirun@gmail.com>
2023-01-14 18:05:12 -0800

    added code TeaDetail.js

---
+++ src/components/TeaDetail.js
@@ -0,0 +1,23 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function TeaDetail(props) {
+  const { tea, onClickingDelete } = props;
+
+  return (
+    <React.Fragment>
+      <h1>Tea Detail</h1>
+      <h3>{tea.name} - {tea.origin} - {tea.price}</h3>
+      <p><em>{tea.roast}</em></p>
+      <hr />
+    </React.Fragment>
+  )
+}
+
+TeaDetail.propTypes = {
+  tea: PropTypes.object,
+  // onClickingDelete: PropTypes.func, // new code
+  // onClickingEdit: PropTypes.func // new code
+};
+
+export default TeaDetail;
