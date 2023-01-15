50d6073402407a60f6a06b1d7631d0cba5c930a5
evegyzbirun <zbirunzbirun@gmail.com>
2023-01-14 19:37:29 -0800

    added all code, server shows white screen

--- src/components/TeaControl.js
+++ src/components/TeaControl.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import NewTeaForm from './NewTeaForm';
 import TeaList from './TeaList';
-import TeaDetail from './TeaDetail';
+//import TeaDetail from './TeaDetail';
 
 class TeaControl extends React.Comment {
 
@@ -16,5 +16,54 @@ class TeaControl extends React.Comment {
   }
 
 
-  
+  handleClick = () => {
+    if (this.state.selectedTea != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedTea: null,
+        editing: false // new code
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+
+  reader() {
+    let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTeaForm onNewTicketCreation={this.handleAddingNewTeaToList} />;
+
+      buttonText = "Return to Tea List";
+    } else {
+      currentlyVisibleState = <TeaList teaList={this.state.mainTeaList} onTeaSelection={this.handleChangingSelectedTea} />;
+
+      buttonText = "Add Tea";
+    }
+
+    return (
+      <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
+      </React.Fragment>
+    );
+  }
+
+  handleAddingNewTeaToList = (newTea) => {
+    const newMainTeaList = this.state.mainTeaList.concat(newTea);
+    this.setState({
+      mainTeaList: newMainTeaList,
+      formVisibleOnPage: false
+    });
+  }
+
+  handleChangingSelectedTea = (id) => {
+    const selectedTea = this.state.mainTeaList.filter(tea => tea.id === id)[0];
+    this.setState({ selectedTea: selectedTea });
+  }
 }
+
+export default TeaControl;
