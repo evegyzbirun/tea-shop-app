555b68e856fa9bf689484927bed020be6bd0c8e2
evegyzbirun <zbirunzbirun@gmail.com>
2023-01-14 17:52:59 -0800

    added props in Tea.js.

---
+++ src/components/Tea.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function Tea(props) {
+  return (
+    <React.Fragment>
+      <div onClick={() => props.whenTicketClicked(props.id)}>
+        <h3>{props.name} - {props.origin} - {props.price}</h3>
+        <p><em>{props.roast}</em></p>
+        <hr />
+
+      </div>
+
+
+    </React.Fragment>
+  )
+}
+
+Tea.propTypes = {
+  name: PropTypes.string,
+  origin: PropTypes.string,
+  price: PropTypes.string,
+  roast: PropTypes.string,
+  //id: PropTypes.string, // new PropType
+  whenTicketClicked: PropTypes.func // new PropType
+};
+
+export default Tea;
